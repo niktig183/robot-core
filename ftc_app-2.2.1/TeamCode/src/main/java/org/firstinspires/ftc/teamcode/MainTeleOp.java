3edc9b70b805555d5b1274718f0fcd48ea3c1d66
Alex Balgavy <a.balgavy@gmail.com>
2017-01-31 16:48:59 +0100

    Moved code for color sensor to a separate file
    For debugging purposes

--- ftc_app-2.2.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ ftc_app-2.2.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -32,16 +32,10 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 package org.firstinspires.ftc.teamcode;
 
-import android.app.Activity;
-import android.graphics.Color;
-import android.view.View;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -74,23 +68,6 @@ public class MainTeleOp extends OpMode {
     DcMotor centerMotor;
     DcMotor highMotor;
 
-    ColorSensor colorSensor;
-    float hsvValues[] = {0F,0F,0F};
-
-    // values is a reference to the hsvValues array.
-    final float values[] = hsvValues;
-
-    // get a reference to the RelativeLayout so we can change the background
-    // color of the Robot Controller app to match the hue detected by the RGB sensor.
-    final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(com.qualcomm.ftcrobotcontroller.R.id.RelativeLayout);
-
-    // buttonPreviousState and buttonCurrentState represent the previous and current state of the button.
-    boolean buttonPreviousState = false;
-    boolean buttonCurrentState = false;
-
-    // buttonLedOn represents the state of the LED.
-    boolean buttonLedOn = true;
-
 
 
     /*
@@ -110,12 +87,6 @@ public class MainTeleOp extends OpMode {
 
         // This thing is useful
         leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        // get a reference to our ColorSensor object.
-        colorSensor = hardwareMap.colorSensor.get("color sensor");
-
-        // Set the LED in the beginning
-        colorSensor.enableLed(buttonLedOn);
 
 
     }
@@ -181,42 +152,6 @@ public class MainTeleOp extends OpMode {
         }
         //endregion
 
-        //region Color Sensor
-        // check the status of the x button on either gamepad.
-        buttonCurrentState = gamepad1.x;
-
-        // check for button state transitions.
-        if ((buttonCurrentState == true) && (buttonCurrentState != buttonPreviousState))  {
-
-            // button is transitioning to a pressed state. So Toggle LED
-            buttonLedOn = !buttonLedOn;
-            colorSensor.enableLed(buttonLedOn);
-        }
-
-        // update previous state variable.
-        buttonPreviousState = buttonCurrentState;
-
-        // convert the RGB values to HSV values.
-        Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
-
-        // send the info back to driver station using telemetry function.
-        telemetry.addData("LED", buttonLedOn ? "On" : "Off");
-        telemetry.addData("Clear", colorSensor.alpha());
-        telemetry.addData("Red  ", colorSensor.red());
-        telemetry.addData("Green", colorSensor.green());
-        telemetry.addData("Blue ", colorSensor.blue());
-        telemetry.addData("Hue", hsvValues[0]);
-
-        // change the background color to match the color detected by the RGB sensor.
-        // pass a reference to the hue, saturation, and value array as an argument
-        // to the HSVToColor method.
-        relativeLayout.post(new Runnable() {
-            public void run() {
-                relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
-            }
-        });
-
-        //endregion
     }
 
     /*
